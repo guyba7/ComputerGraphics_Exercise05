3fc8bbb71d75a9fcb9716c579dbd680dc62a550a
Guy Ben Ari <guyba7@gmail.com>
2025-06-24 16:05:29 +0300

    adjusted direction light intensity.

--- src/hw5.js
+++ src/hw5.js
@@ -13,7 +13,7 @@ scene.background = new THREE.Color(0x000000);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff,1);
+const directionalLight = new THREE.DirectionalLight(0xffffff,0.8);
 directionalLight.position.set(10, 20, 15);
 
 scene.add(directionalLight);
